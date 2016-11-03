8d6e184b9072ba879b918da2cbabcd09e4c29e2b
pabloprog3 <pablo_h_n@yahoo.com.ar>
2016-11-03 07:48:13 -0300

    planteo de formularios y diseño de las tablas en la BD

---
+++ login.php
@@ -0,0 +1,15 @@
+<form method="post">
+	<div class="form-group"  style="width:250px" >
+		<h2>Ingrese sus datos</h2>
+
+		<input type="mail" class="form-control" name="correo" id="correo" placeholder="Correo Electrónico">
+		<br>
+		<input type="password" class="form-control" name="clave" id="clave" placeholder="Clave">
+		<br>
+		<input type="checkbox" class="checkbox-inline" name="recordarme" id="recordarme" checked>Recordarme
+		<br><br>
+		<input type="button" class="btn btn-primary btn-block" name="Ingresar" id="Ingresar" value="Ingresar">
+		<br>
+	</div>
+
+</form>
