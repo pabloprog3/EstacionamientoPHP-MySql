aa225128f0034e0eb15401183a370ff85587ba1b
pabloprog3 <pablo_h_n@yahoo.com.ar>
2016-11-14 12:18:36 -0300

    Agrego los datos de conexion hostinger

--- Conexion.php
+++ Conexion.php
@@ -5,9 +5,9 @@ class AccesoPDO
 	
 	public function getConexion()
 	{
-		$user = "root";
-		$pass = "";
-		$host = "localhost";
+		$user = "u165823463_pablo";
+		$pass = "utnfra2003";
+		$host = "mysql.hostinger.com.ar";
 		$db = "estacionamiento";
 
 		$conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
